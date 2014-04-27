63afd45baf925ef7928f861935111ec4998a32f6
Judas-Iscariot <j.iscariot@israelmail.com>
2014-04-28 01:36:18 +0200

    renaming

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("JHC");
+    case mBTC: return QString("mJHC");
+    case uBTC: return QString::fromUtf8("μJHC");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Jesuscoins");
+    case mBTC: return QString("Milli-Jesuscoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Jesuscoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
